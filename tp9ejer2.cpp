2f98ec3a624bd15eea21b5e49b60238bd5585c2c
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-10-11 10:51:49 -0300

    Add files via upload

---
+++ tp9ejer2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Alumno{
+	string nomApe;
+	int dni;
+	char sexo;
+};
+struct Registro{
+	Alumno estudiante;
+	double notaProm[5];
+	int amon[5];
+	int faltas[5];
+};
+
+void cargarDatos(Registro alumnos[20]){
+	int i = 0;
+	int j = 0;
+	for (i=1;i<=20;i++) {
+		cout<<"Ingrese nombre y apellido del alumno "<<i<<endl;
+		getline (cin,alumnos[i].estudiante.nomApe);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	Registro alumnos[20];
+	cargarDatos(alumnos);
+	return 0;
+	
+}
+
